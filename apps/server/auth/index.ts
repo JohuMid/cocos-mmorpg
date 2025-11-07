9b74879707cdac9163944c11fd2c4711eed522cc
JohuMid <johumid@outlook.com>
2025-11-07 15:19:13 +0800

    feat(auth): 实现用户注册功能并连接数据库。

--- apps/server/auth/index.ts
+++ apps/server/auth/index.ts
@@ -1,9 +1,21 @@
 console.log("auth 服务");
 import bodyParser from "body-parser";
 import express from "express";
+import mysql from "mysql";
+import dayjs from "dayjs";
 // @ts-ignore
 import Crypto from "node-jsencrypt";
 import { PrivateKey } from "../common";
+
+import { createHash } from "crypto";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "mmodb"
+});
+connection.connect();
 
 const Crypt = new Crypto();
 
@@ -17,13 +29,29 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/register", (req, res) => {
-  let {account, password} = req.body;
+  let { account, password } = req.body;
   account = Crypt.decrypt(account);
   password = Crypt.decrypt(password);
   console.log(account);
   console.log(password);
-  
-  res.send({});
+
+  const hash = createHash("md5");
+  hash.update(password);
+  const passwordHash = hash.digest("hex");
+
+  connection.query(
+    "INSERT INTO user (account, password,created_time) VALUES (?, ?,?)",
+    [account, passwordHash, dayjs().format("YYYY-MM-DD HH:mm:ss")],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: "Internal server error" });
+        return;
+      }
+      console.log(results);
+      res.send({});
+    }
+  );
 });
 
 app.listen(3000, () => {
